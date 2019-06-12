fed89d5c396fb57af74a8fceb64379a2641fee4a
nandiniprashar <51734269+nandiniprashar@users.noreply.github.com>
2019-06-12 13:02:21 +0530

    Add files via upload

---
+++ js/submit.js
@@ -0,0 +1,23 @@
+function validateform()
+{
+	var name=document.myform.name.value;
+	var x=document.myform.email.value;
+	var atposition=x.IndexOf("@");
+	var dotposition=x.IndexOf(".");
+	if(name==null||name=="")
+	{
+		alert="Please fill your name";
+		return false;
+	}
+	else
+	{
+	console.log("welcome"+name);	
+	}
+	if(atposition<1||dotposition<atposition+2||dotposition+2>x.length)
+	{
+		alert("please enter a valid email address");
+		return false;
+		
+	}
+
+}
